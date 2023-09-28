2c4ea7cfb123a6f1dec58007a7ba298be16106df
framadhan99 <118242446+framadhan99@users.noreply.github.com>
2023-09-28 09:31:49 +0700

    Update DetailViewModel.kt

--- app/src/main/java/com/fajar/submissiongitapp/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/fajar/submissiongitapp/ui/viewmodel/DetailViewModel.kt
@@ -10,17 +10,7 @@ import com.fajar.submissiongitapp.core.data.detail.DetailResponse
 import kotlinx.coroutines.launch
 
 class DetailViewModel(application: Application) : AndroidViewModel(application) {
-    private val api = ApiService.create()
-
-    private val _user = MutableLiveData<DetailResponse>()
-    val user: LiveData<DetailResponse> = _user
-
-    private val _username = MutableLiveData<String>()
-    val username: LiveData<String> = _username
-
-    private val _errorMsg = MutableLiveData<String>()
-    val errorMsg: LiveData<String> = _errorMsg
-
+   
     fun getDetailUser(username: String) {
         try {
             viewModelScope.launch {
